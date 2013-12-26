72260f4c964f652d4d844418a0e79512c1c30575
Georgi Baghdasaryan <baghdasaryan@ucla.edu>
2013-12-25 17:46:40 -0800

    added GLutils and function prototypes for Drawer

---
+++ opengl-objects/GLutils.h
@@ -0,0 +1,54 @@
+// ############################################ //
+// ############################################ //
+// ####                                    #### //
+// ####    Program: OpenGL-Objects         #### //
+// ####    Language: C++                   #### //
+// ####    Author: Georgi Baghdasaryan     #### //
+// ####    Email: baghdasaryan@ucla.edu    #### //
+// ####                                    #### //
+// ############################################ //
+// ############################################ //
+
+
+#ifndef __GLUTILS_H__
+#define __GLUTILS_H__
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+#ifdef __APPLE__  // Apple 
+#  include <OpenGL/OpenGL.h>
+#  include <GLUT/glut.h>
+#elif __linux__ // Linux 
+#  include "GL/glew.h"
+#  include "GL/freeglut.h"
+#else // Windows
+#  include <windows.h> 
+#  include "GL/glew.h"
+#  include "GL/freeglut.h"
+#endif
+
+#include "Eigen/Dense"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#ifndef DEGREES_TO_RADIANS
+#define DEGREES_TO_RADIANS 0.01745329251994329576922222222222	// Same as (M_PI / 180.0)
+#endif
+
+#ifndef GRAVITY_ACC
+#define GRAVITY_ACC 9.80665
+#endif
+
+// Blah blah blah
+namespace GLutils
+{
+	GLuint InitShader(const char* vShaderFile, const char* fShaderFile);
+}
+
+using namespace GLutils;
+
+#endif  // __GLUTILS_H__
--- opengl-objects/ObjectManager.h
+++ opengl-objects/ObjectManager.h
@@ -13,5 +13,23 @@
 #ifndef __OBJECTMANAGER_H__
 #define __OBJECTMANAGER_H__
 
+#include "Eigen/Dense"
+
+class Drawer
+{
+public:
+	void RotateX(double angle);
+	void RotateY(double angle);
+	void RotateZ(double angle);
+	void Rotate(const Eigen::Vector3f &rotation);
+
+	void Scale(double scale);
+	void Scale(const Eigen::Vector3f &scale);
+
+	void Translate(const Eigen::Vector3f& a_translation);
+
+private:
+	
+};
 
 #endif  // __OBJECTMANAGER_H__
